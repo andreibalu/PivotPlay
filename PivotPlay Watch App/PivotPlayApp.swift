0c3174ee5042393a08ff9571be952e143752bfb9
Andrei <andrei.baluta@yahoo.com>
2025-07-10 16:26:25 +0300

    Initial Commit

---
+++ PivotPlay Watch App/PivotPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PivotPlayApp.swift
+//  PivotPlay Watch App
+//
+//  Created by andrei on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PivotPlay_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PivotPlay/PivotPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PivotPlayApp.swift
+//  PivotPlay
+//
+//  Created by andrei on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PivotPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
